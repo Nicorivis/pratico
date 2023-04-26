1afd7a169366284fb26e9ed0c63d1f8020d9cb4d
Nicolas de Paula Perim <79946727+Nicorivis@users.noreply.github.com>
2023-04-26 19:35:00 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const https = require('https');
+
+https.get('https://api.otaviolube.com/api/filmes', (resp) => {
+  let data = '';
+
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  resp.on('end', () => {
+    console.log(JSON.parse(data));
+  });
+
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
